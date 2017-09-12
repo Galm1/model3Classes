2902aa389678beb401d819e855a864c9b3523d76
EliGarcia <garciaelisha93@yahoo.com>
2017-09-12 11:03:55 -0500

    class for school not complete

--- src/com/theironyard/installparty/school.java
+++ src/com/theironyard/installparty/school.java
@@ -1,4 +1,17 @@
 package com.theironyard.installparty;
 
 public class school {
+    String name;
+    int students;
+    boolean killingIt;
+
+    public school (String name, int students, boolean killingIt){
+        this.name = name;
+        this.students = students;
+        this.killingIt = killingIt;
+    }
+    public String schoolDetails(){
+        return 
+    }
 }
+
